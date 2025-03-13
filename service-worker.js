dd007666ecd19e10681fb5afc19ff369c7fa5631
salvacam <salvacams@gmail.com>
2025-03-13 19:32:02 +0100

    PWA

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+var cacheName = 'camelot_jsdos-v0.0.90';
+
+var filesToCache = [
+  './',
+  './index.html',
+  './js-dos.js',
+  './js-dos.js.map',
+  './js-dos.css',  
+  './camelot.jsdos',  
+  './emulators/emulators.js',
+  './emulators/emulators.js.map',
+  './emulators/wlibzip.wasm',
+  './emulators/wdosbox.wasm',
+  './emulators/wdosbox.js',
+  './emulators/wdosbox.js.symbols',
+  './emulators/wlibzip.js',
+  './img/favicon.ico'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install_');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate_');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key.startsWith('polen-')){
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
